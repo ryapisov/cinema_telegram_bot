b15914fbd3ddc09188c49defd901005d2196d361
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-09-01 17:15:53 +0300

    add key board

--- src/helper.js
+++ src/helper.js
@@ -3,5 +3,9 @@ const log = console.log
 module.exports = {
   logStart(msg = ''){
     log('Bot has been started ...\n', msg)
+  },
+
+  getChatId(msg){
+    return msg.chat.id
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ require('dotenv').config()
 const Telegram = require("node-telegram-bot-api")
 const config = require("./config")
 const helper = require("./helper.js")
+const keyboard = require("./keyboard")
+const kb = require("./keyboard-buttons")
 
 const bot = new Telegram(config.TOKEN, {polling:true})
 helper.logStart()
 
 bot.on('message', msg =>{
   console.log(msg)
+  const chatId = helper.getChatId(msg)
+
+  switch (msg.text) {
+    case kb.home.favourite:
+      break
+    case kb.home.films:
+      bot.sendMessage(chatId, 'Выберите жанр', {
+        reply_markup:{keyboard: keyboard.films}
+      })
+      break
+    case kb.home.cinemas:
+      break
+    case kb.back:
+      bot.sendMessage(chatId, 'Что хотите посмотреть?', {
+        reply_markup:{keyboard: keyboard.home}
+      })
+      break
+  }
+
+})
+
+bot.onText(/\/start/, msg => {
+  const text = `Hi!, ${msg.from.first_nsme} \nКоманда:`
+
+  bot.sendMessage(helper.getChatId(msg),  text, {
+    reply_markup:{
+      keyboard: keyboard.home
+    }
+  })
 })
